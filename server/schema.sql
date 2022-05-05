3d983876452d735dcaf8144960127137c2ee9c34
skydodle <skydodle@gmail.com>
2022-05-04 19:33:35 -0500

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,21 @@
+drop database if exists chat;
 CREATE DATABASE chat;
-
 USE chat;
+
+CREATE TABLE users (
+  id integer(11) auto_increment,
+  username varchar(20),
+  PRIMARY KEY (id)
+);
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id integer(11) auto_increment,
+  message varchar(200),
+  username_id integer(11), /*foreign key*/
+  PRIMARY KEY (id),
+  FOREIGN KEY (username_id) REFERENCES users(id)
 );
-
-/* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
